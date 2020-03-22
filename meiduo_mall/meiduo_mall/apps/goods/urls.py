250e02639c9f169109dd1e4703d654e80648d122
xinglejintianzhen <xinglejintianzhen@126.com>
2020-03-22 14:34:27 +0800

    商品展示-统计分类商品的访问量完毕

--- meiduo_mall/meiduo_mall/apps/goods/urls.py
+++ meiduo_mall/meiduo_mall/apps/goods/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     # 商品详情
     re_path(r'^detail/(?P<sku_id>\d+)/$', views.DetailView.as_view(), name='detail'),
     # 统计分类商品的访问量
-    # re_path(r'^detail/visit/(?P<category_id>\d+)/$', views.DetailVisitView.as_view()),
+    re_path(r'^detail/visit/(?P<category_id>\d+)/$', views.DetailVisitView.as_view()),
 ]
 
